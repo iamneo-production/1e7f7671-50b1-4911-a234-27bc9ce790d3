fa1e33ddb7f266910168d12c5e2ee53d92348882
jashu0692 <jashwanth600@gmail.com>
2023-07-14 10:14:26 +0000

    added questions to the queries

--- jashu.sql
+++ jashu.sql
@@ -2,17 +2,17 @@ select * from BLOOD_BANK;
 
 desc BLOOD_BANK;
 
---1 total count of blood banks in each state[executed]
+--1 SQL query to find the total count of blood banks in each state[executed]
 select state,count("Blood Bank Name") from BLOOD_BANK group by state;
 
---2 blood banks in AP state[executed]
+--2 SQL query to list out the blood banks in Andhra Pradesh state[executed]
 select "Blood Bank Name" from BLOOD_BANK where state='Andhra Pradesh';
 
---3 total blood banks in dataset[executed]
+--3 SQL query to find the total blood banks in the dataset[executed]
 select count("Blood Bank Name") from BLOOD_BANK;
 
---4 first 5 rows[executed]
+--4 SQL query to display the first 5 blood banks names in ascending order[executed]
 select "Blood Bank Name" from BLOOD_BANK order by "Blood Bank Name" fetch first 5 rows only;
 
---5 blood banks in alphabetical order[executed]
+--5 SQL query to list the blood bank names in alphabetical order[executed]
 select "Blood Bank Name" from BLOOD_BANK order by "Blood Bank Name" asc;
