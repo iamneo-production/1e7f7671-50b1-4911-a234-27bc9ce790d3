c48ee141617fc28fd4c682fe01d122bce875ceca
jayanvi <jayanvinekkalapudi@gmail.com>
2023-07-17 05:35:46 +0000

    executed queries

--- jayanvi.sql
+++ jayanvi.sql
@@ -4,8 +4,9 @@ desc BLOOD_BANK;
 /*write a sql query to find 
 the total count of blood banks in each state*/
  
-SELECT STATE, COUNT("Blood Bank Name") FROM BLOOD_BANK
-  GROUP BY STATE;
+SELECT STATE, COUNT("Blood Bank Name") AS CountofBloodBanks 
+  FROM BLOOD_BANK
+  GROUP BY STATE ORDER BY STATE;
 
 /* write a sql query to list out blood banks in Andhra Pradesh state*/
 
@@ -14,7 +15,7 @@ SELECT "Blood Bank Name" FROM BLOOD_BANK
 
 /* write a sql query to find total blood banks in dataset */
 
-SELECT COUNT("Blood Bank Name") FROM BLOOD_BANK;
+SELECT COUNT("Blood Bank Name") AS TOTALBLOODBANKS FROM BLOOD_BANK;
 
 /* write a sql query to display first five blood banks in ascending order*/
 
@@ -25,6 +26,7 @@ SELECT "Blood Bank Name" FROM BLOOD_BANK ORDER BY "Blood Bank Name" ASC
 
  SELECT "Blood Bank Name" FROM BLOOD_BANK 
   ORDER BY "Blood Bank Name" ASC;
+  
   
 
   
