c34ca015851dadf039aafa09e2e51c55248d32c6
Kamalakar1905 <kamalakarreddy2001@gmail.com>
2023-07-17 06:23:55 +0000

    Sql

---
+++ kamalakar.sql
@@ -0,0 +1,9 @@
+SELECT state, count(*) from blood_bank group by state;
+
+select *  from blood_bank where state='Andhra Pradesh';
+
+select count(*) as total_count from blood_bank;
+
+select * from blood_bank where rownum<=5 order by 'blood bank name';
+
+select "Blood Bank Name" from blood_bank order by "Blood Bank Name";
