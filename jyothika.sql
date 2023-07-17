6c0aea1ed5c8a7cbfc3f37843a8092c37ec9099a
jyothikavalli <jyli.kola88@gmail.com>
2023-07-17 02:46:05 +0000

    executed queries

--- jyothika.sql
+++ jyothika.sql
@@ -2,6 +2,7 @@
 --1
 --select state,count("Blood Bank Name") as "Number of Blood Banks" from BLOOD_BANK group by state order by state;
 --select state,count("Blood Bank Name") from BLOOD_BANK group by state;
+--select state,count(*) from BLOOD_BANK group by state;
 select state,count(id) from BLOOD_BANK group by state;
 
 --2
@@ -10,6 +11,7 @@ select "Blood Bank Name" from BLOOD_BANK where state = 'Andhra Pradesh';
 --3
 --select count("Blood Bank Name") as "Total no. of blood banks" from BLOOD_BANK;
 --select count("Blood Bank Name") from BLOOD_BANK;
+--select count(*) from BLOOD_BANK;
 select count(id) from BLOOD_BANK;
 
 --4
