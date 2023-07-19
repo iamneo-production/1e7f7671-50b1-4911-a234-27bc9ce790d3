ba2c27cece555da9612c7ec312356a1e8c0d135e
Hasitha711 <hasithavidya@gmail.com>
2023-07-19 16:22:57 +0000

    Queries

--- Hasitha.sql
+++ Hasitha.sql
@@ -1,5 +1,5 @@
 --1
-SELECT state,count(*) 
+SELECT state,count("Blood Bank Name") 
 FROM BLOOD_BANK GROUP BY state;
 
 --2
@@ -7,7 +7,7 @@ SELECT "Blood Bank Name" FROM BLOOD_BANK
 WHERE State='Andhra Pradesh';
 
 --3
-SELECT COUNT(*) FROM BLOOD_BANK;
+SELECT COUNT("Blood Bank Name") FROM BLOOD_BANK;
 
 --4
 SELECT * FROM(SELECT "Blood Bank Name" FROM BLOOD_BANK ORDER BY "Blood Bank Name") WHERE rownum<=5;
