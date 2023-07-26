112b97ef27d08ddd1e7bb9fa037b70788fda3b6a
kaviyanjali <kaviyanjali342002@gmail.com>
2023-07-26 17:36:50 +0000

    Backup Commit

---
+++ kaviyanjali.sql
@@ -0,0 +1,21 @@
+desc blood_bank;
+--1  Find the total count of blood banks in each state
+
+select state,count(id) from blood_bank group by state;
+
+--2 List out the blood banks in Andhra Pradesh state
+
+select "Blood Bank Name" from blood_bank where state='Andhra Pradesh';
+
+--3 Find the total blood banks in the dataset 
+
+select count('Blood Bank Name') from blood_bank;
+
+-- 4 Display the first 5 blood banks names in ascending order
+
+select * from blood_bank where rownum <=5 order by 'Blood Bank Name';
+
+--5 List the blood bank names in a alphabetical order
+
+select "Blood Bank Name" from blood_bank order by 'Blood Bank Name' asc;
+  
