3b3d97cdb26041023764becb2023c69e51d78e69
jyothikavalli <jyli.kola88@gmail.com>
2023-07-28 12:51:42 +0000

    executed queries

--- jyothikavalli_kola.sql
+++ jyothikavalli_kola.sql
@@ -10,9 +10,9 @@ select "Blood Bank Name" from BLOOD_BANK where state = 'Andhra Pradesh';
 
 --3
 --select count("Blood Bank Name") as "Total no. of blood banks" from BLOOD_BANK;
---select count("Blood Bank Name") from BLOOD_BANK;
+select count("Blood Bank Name") from BLOOD_BANK;
 --select count(*) from BLOOD_BANK;
-select count(id) from BLOOD_BANK;
+--select count(id) from BLOOD_BANK;
 
 --4
 --select "Blood Bank Name" from BLOOD_BANK order by "Blood Bank Name" fetch first 5 rows only;
