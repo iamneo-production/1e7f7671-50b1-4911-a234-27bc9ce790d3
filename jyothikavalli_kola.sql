6b2b6eadbf0c33e7ecc5fbc1db4c265591c33d26
jyothikavalli <jyli.kola88@gmail.com>
2023-07-28 13:39:45 +0000

    .

--- jyothikavalli_kola.sql
+++ jyothikavalli_kola.sql
@@ -1,7 +1,6 @@
 --desc blood_bank;
 
 --1
-
 --select state,count("Blood Bank Name") from BLOOD_BANK group by state;
 --select state,count(*) from BLOOD_BANK group by state;
 select state,count(id) from BLOOD_BANK group by state;
