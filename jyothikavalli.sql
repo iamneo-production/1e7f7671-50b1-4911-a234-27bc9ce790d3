1b9fbb4e5d5f559e63ee4b6bf5b2552e86888c93
jyothikavalli <jyli.kola88@gmail.com>
2023-07-29 14:13:13 +0000

    commit

--- jyothikavalli.sql
+++ jyothikavalli.sql
@@ -8,13 +8,13 @@ select state,count(id) from BLOOD_BANK group by state;
 select "Blood Bank Name" from BLOOD_BANK where state = 'Andhra Pradesh';
 
 --3
---select count("Blood Bank Name") from BLOOD_BANK;
+select count("Blood Bank Name") from BLOOD_BANK;
 --select count(*) from BLOOD_BANK;
-select count(id) from BLOOD_BANK;
+s--elect count(id) from BLOOD_BANK;
 
 --4
 --select "Blood Bank Name" from BLOOD_BANK order by "Blood Bank Name" asc fetch first 5 rows only;
-select * from(select "Blood Bank Name" from BLOOD_BANK order by "Blood Bank Name")where rownum <= 5;
+select * from(select "Blood Bank Name" from BLOOD_BANK order by "Blood Bank Name" asc)where rownum <= 5;
 
 --5
 select "Blood Bank Name" from BLOOD_BANK order by "Blood Bank Name" ASC;
