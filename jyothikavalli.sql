b84b1137597130aca055b7ef016aa78a7f154d1d
jyothikavalli <jyli.kola88@gmail.com>
2023-07-29 12:33:07 +0000

    .

--- jyothikavalli.sql
+++ jyothikavalli.sql
@@ -13,8 +13,8 @@ select "Blood Bank Name" from BLOOD_BANK where state = 'Andhra Pradesh';
 select count(id) from BLOOD_BANK;
 
 --4
---select "Blood Bank Name" from BLOOD_BANK order by "Blood Bank Name" fetch first 5 rows only;
-select * from(select "Blood Bank Name" from BLOOD_BANK order by "Blood Bank Name")where rownum <= 5;
+select "Blood Bank Name" from BLOOD_BANK order by "Blood Bank Name" fetch first 5 rows only;
+--select * from(select "Blood Bank Name" from BLOOD_BANK order by "Blood Bank Name")where rownum <= 5;
 
 --5
-select "Blood Bank Name" from BLOOD_BANK order by "Blood Bank Name";
+select "Blood Bank Name" from BLOOD_BANK order by "Blood Bank Name" ASC;
