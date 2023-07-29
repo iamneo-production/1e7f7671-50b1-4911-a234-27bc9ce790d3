41fc6755bea25aa0da210a3d417e77c42efeda58
Kamalakar1905 <kamalakarreddy2001@gmail.com>
2023-07-29 16:14:41 +0000

    kamalakar

--- kamalakar.sql
+++ kamalakar.sql
@@ -1,9 +1,22 @@
+/* Sql performance queries*/
+
+
+/* Write a SQL Query to find the total count of blood bank in each state */
+
 SELECT state, count(*) from blood_bank group by state;
+
+/* Write a SQL Query to list out the blood banks in Andhra Pradesh */
 
 SELECT * from blood_bank where state='Andhra Pradesh';
 
+/*Write a SQL Query to find the total blood banks in dataset */
+
 SELECT count(*) as total_count from blood_bank;
 
-SELECT * from blood_bank where rownum<=5 order by 'blood bank name';
+/*Wirte a SQL Query to display the first 5 blood banks '  names in ascending order */
+
+SELECT * from blood_bank where rownum<=5 order by 'Blood Bank name';
+
+/*Write a SQL Query to list the blood bank names in alphabetical order */
 
 SELECT "Blood Bank Name" from blood_bank order by "Blood Bank Name";
