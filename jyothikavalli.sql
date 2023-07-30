da5c908496dee4004430588cb1715fa949c5e107
jyothikavalli <jyli.kola88@gmail.com>
2023-07-30 15:58:06 +0000

    comitted

--- jyothikavalli.sql
+++ jyothikavalli.sql
@@ -2,7 +2,7 @@
 --desc blood_bank;
 --1
 --select state,count("Blood Bank Name") from BLOOD_BANK group by state;
---select state,count(*) from BLOOD_BANK group by state;
+
 select state,count(id) from BLOOD_BANK group by state;
 
 --2
